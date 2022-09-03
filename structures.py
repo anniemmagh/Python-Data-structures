151339aebbc1a0e548e8440986e519e1f5427633
anniemmagh <91937613+anniemmagh@users.noreply.github.com>
2022-09-04 01:09:53 +0400

    add datas in the list -append()

--- structures.py
+++ structures.py
@@ -1,24 +1,25 @@
 # List 
-x = 1
-myList = [1,2,3,4]
-print(myList)
+# x = 1
+# myList = [1,2,3,4]
+# print(myList)
 
 # allows differtent data types
-mmylist =[1,2,"Ani","Megi", True,False, 3.0]
-name = mmylist[3] 
-print(name)
+# mmylist =[1,2,"Ani","Megi", True,False, 3.0]
+# name = mmylist[3] 
+# print(name)
 
-#changeable - List-ის თვისება
-# List allows Duplicates
-mmylist[1] = 1
-print(mmylist)
-n = len(mmylist)
-print(mmylist[n-1]) # Get last data
+# #changeable - List-ის თვისება
+# # List allows Duplicates
+# mmylist[1] = 1
+# print(mmylist)
+# n = len(mmylist)
+# print(mmylist[n-1]) # Get last data
 # print(mmylist[len(mmylist)-1]) # Get last data
-
-
-
-
 
 # print(mmylist)
 # print(len(mmylist)) 
+
+# add datas in the list
+newList =[1,2,"Ani","Megi", True,False, 3.0, 10,1000,"Maghlakvelidze", "Georgia"]
+newList.append("Tbilisi")
+print(newList)
