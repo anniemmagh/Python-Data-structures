4ba1dedc0f240ad9c71df4b4556a13927e6d2dd5
anniemmagh <91937613+anniemmagh@users.noreply.github.com>
2022-09-04 00:53:11 +0400

    index

--- structures.py
+++ structures.py
@@ -5,4 +5,9 @@ print(myList)
 
 # allows differtent data types
 mmylist =[1,2,"Ani","Megi", True,False, 3.0]
-print(mmylist)
+name = mmylist[3] 
+print(name)
+
+
+# print(mmylist)
+# print(len(mmylist)) 
