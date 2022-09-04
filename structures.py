5c9becfd1a164bd9e98b515512f68f51b3ec5666
anniemmagh <91937613+anniemmagh@users.noreply.github.com>
2022-09-04 16:56:06 +0400

    tuple data structure

--- structures.py
+++ structures.py
@@ -1,42 +1,50 @@
-# List -data structure
-x = 1
-myList = [1,2,3,4]
-print(myList)
+# # List -data structure
+# x = 1
+# myList = [1,2,3,4]
+# print(myList)
 
-# allows differtent data types
-# mmylist =[1,2,"Ani","Megi", True,False, 3.0]
-# name = mmylist[3] 
-# print(name)
+# # allows differtent data types
+# # mmylist =[1,2,"Ani","Megi", True,False, 3.0]
+# # name = mmylist[3] 
+# # print(name)
 
-# #changeable - List-ის თვისება
-# # List allows Duplicates
-# mmylist[1] = 1
-# print(mmylist)
-# n = len(mmylist)
-# print(mmylist[n-1]) # Get last data
-# print(mmylist[len(mmylist)-1]) # Get last data
+# # #changeable - List-ის თვისება
+# # # List allows Duplicates
+# # mmylist[1] = 1
+# # print(mmylist)
+# # n = len(mmylist)
+# # print(mmylist[n-1]) # Get last data
+# # print(mmylist[len(mmylist)-1]) # Get last data
 
-# print(mmylist)
-# print(len(mmylist)) 
+# # print(mmylist)
+# # print(len(mmylist)) 
 
-# add datas in the list
-# newList =[1,2,"Ani","Megi", True,False, 3.0, 10,1000,"Maghlakvelidze", "Georgia"]
-# newList.append("Tbilisi") # add item
-# newList.pop(5)  # delete item
-# newList.remove("Georgia") # Remove item
-# print(newList)
+# # add datas in the list
+# # newList =[1,2,"Ani","Megi", True,False, 3.0, 10,1000,"Maghlakvelidze", "Georgia"]
+# # newList.append("Tbilisi") # add item
+# # newList.pop(5)  # delete item
+# # newList.remove("Georgia") # Remove item
+# # print(newList)
 
-# set - data structure
-mySet = {1,2,"Ani","asus",1} # duplicates gonna delete
-# names = ["Marta" , "Giorgi" ,"Ani", "Marta"]
-# names_set = set(names)
-# print(names_set)
-mySet.add(120)
-mySet.remove("Ani")
+# # set - data structure
+# mySet = {1,2,"Ani","asus",1} # duplicates gonna delete
+# # names = ["Marta" , "Giorgi" ,"Ani", "Marta"]
+# # names_set = set(names)
+# # print(names_set)
+# mySet.add(120)
+# mySet.remove("Ani")
 
-print(mySet)
-numbers = {1,10,100}
-nnames = {"Ani", "Marta" , "Giorgi"}
-numbers_names = numbers.union(nnames)
-numbers.update(nnames)
-print(numbers_names)
+# print(mySet)
+# numbers = {1,10,100}
+# nnames = {"Ani", "Marta" , "Giorgi"}
+# numbers_names = numbers.union(nnames)
+# numbers.update(nnames)
+# print(numbers_names)
+
+
+# tuple
+#allows different data types & duplicate 
+# ordered and unchangeable 
+myTuple = (1,2, "Test 1" , True,2,1)
+
+print(myTuple)
